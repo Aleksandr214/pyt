571f0ace7075d37ccb26efafd8ea91285db7f213
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2024-01-27 22:14:45 +0400

    сохронил

--- main.py
+++ main.py
@@ -4,8 +4,3 @@
 # n="qwer"
 # print(n)
 # b="qwerty "
-
-a=int(input("Введите первое число:"))
-b=int(input("Введите второе число:"))
-c=int(input("Введите третие число:"))
-print(a,"+",b,'+',c,'=',a+b+c)
